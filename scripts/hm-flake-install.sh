37c3e363bb5a2469267064484ae2ccffb73ad45f
dnkmmr <104856296+dnkmmr69420@users.noreply.github.com>
2023-07-10 09:39:37 -0500

    Update hm-flake-install.sh

--- scripts/hm-flake-install.sh
+++ scripts/hm-flake-install.sh
@@ -4,7 +4,8 @@ if (( $EUID != 0 )); then
     echo "Must be ran as root"
     exit 1
 fi
-
-mkdir -p /etc/home-manager
-ln -s /etc/home-manager /root/.config/home-manager
+rm -rf /etc/home-manager
 nix run home-manager/master -- init --switch
+mv /root/.config/home-manager /etc/home-manager
+ln -s /etc/home-manager /root/.config/home-manager
+
